d5f0b529fc173d1d310e1fa8273ddc93a9f85c97
Abdul Moiz Siddiqui <abdulmoizsiddiqui001@gmail.com>
2022-07-28 05:45:27 +0500

    bmi rounding to 2

--- Day 3 - Conditional game/Advanced BMI conditions.py
+++ Day 3 - Conditional game/Advanced BMI conditions.py
@@ -5,18 +5,17 @@ weight = float(input("enter your weight in kg: "))
 
 #Write your code below this line 👇
 
-bmi = weight/height**2
-print(round(bmi,2))
+bmi = round(weight/height**2,2)
 
 
 if bmi <= 18.5:
-  print("You are underweight")
+  print(f"Your BMI is {bmi} - You are underweight")
 elif bmi <= 25:
-  print("You have a normal weight ")
+  print(f"Your BMI is {bmi} - You have a normal weight ")
 elif bmi <= 30:
-  print("You are overweight ")
+  print(f"Your BMI is {bmi} - You are overweight ")
 elif bmi <= 35:
-  print("You are obese")
+  print(f"Your BMI is {bmi} - You are obese")
 else:
-  print("Alert! Clinically Obese!")
+  print(f"Your BMI is {bmi} - Alert! Clinically Obese!")
   

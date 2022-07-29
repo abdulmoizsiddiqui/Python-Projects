053e7c535fc86f2b5e88572de84ea50d88b64fb9
Abdul Moiz Siddiqui <abdulmoizsiddiqui001@gmail.com>
2022-07-29 15:02:51 +0500

    Roller Coaster

--- Day 3 - Conditional game/Roller Coaster Condition.py
+++ Day 3 - Conditional game/Roller Coaster Condition.py
@@ -1,7 +1,17 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+age = int(input("What is yor age? "))
 
 if height >= 120:
     print("Can ride that Roller Coaster!")
+    if age <12:
+        bill = 5
+        print(f"Child tickets are ${bill}")
+    elif age <= 18:
+        bill = 7
+        print(f"Youth tickets are ${bill}")
+    elif age > 45 and age < 55:
+        bill = 0
+        print("The bill's on us! Enjoy your ride :)")
 else:
     print("Cant ride Roller Coaster :(")

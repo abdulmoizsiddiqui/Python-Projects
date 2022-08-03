dc2df43e031e771e3b456bb1d52e4081d0966382
Abdul Moiz Siddiqui <abdulmoizsiddiqui001@gmail.com>
2022-08-03 05:54:02 +0500

    Cyborg

---
+++ Day 6 - Functions and While Loops/CyborgGame.py
@@ -0,0 +1,22 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def taapo():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+
+for item in range(0,6):
+    taapo()
+print(item)

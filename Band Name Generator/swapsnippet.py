48a5c6aac0ec4f0dfc22a6a1693ea51abe4d3a98
Abdul Moiz Siddiqui <abdulmoizsiddiqui001@gmail.com>
2022-11-20 08:40:03 -0800

    space

--- Band Name Generator/swapsnippet.py
+++ Band Name Generator/swapsnippet.py
@@ -2,7 +2,6 @@ starting_position = int(input('Please enter the starting range: \n'))
 ending_position = int(input('Please enter the ending range: \n'))
 
 if ending_position < starting_position:
-
     # swapping the values
     swap = starting_position
     starting_position = ending_position

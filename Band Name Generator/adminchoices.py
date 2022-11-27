a4e2ffc159dfaf18ea0dd2d3e387dd0900c74439
Abdul Moiz Siddiqui <abdulmoizsiddiqui001@gmail.com>
2022-11-27 07:03:34 -0800

    shakal

--- Band Name Generator/adminchoices.py
+++ Band Name Generator/adminchoices.py
@@ -1,19 +1,19 @@
-choice= input("Choose what function you want me to perform: \n")
+choice= input("Choose what function you want me to perform:")
 exit= False
 
+if choice == '1':
+    print("Hello:")
+elif choice == '2':
+    print("This is second choice")
+elif choice == '3':
+    print("This is the third choice")
+elif choice == '4':
+    print("This the 4th choice")
+elif choice == '5':
+    print("This is 5th choice")
+else:
+    print("You chose an invalid choice")
 
-while exit != False:
-    print("Do you want to perform another tasks?")
-    if choice == '1':
-        print("Hello:")
-    elif choice == '2':
-        print("This is second choice")
-    elif choice == '3':
-        print("This is the third choice")
-    elif choice == '4':
-        print("This the 4th choice")
-    elif choice == '5':
-        print("This is 5th choice")
-    else:
-        print("You chose an invalid choice")
-
+while choice != 'exit':
+    print("Do you want to perform another task?")
+    choice= input("Choose what function you want me to perform:")

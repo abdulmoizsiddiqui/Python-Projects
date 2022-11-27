4c440817632e86aba2b396a7408e8db04974f7fd
Abdul Moiz Siddiqui <abdulmoizsiddiqui001@gmail.com>
2022-11-27 07:00:06 -0800

    sa

--- Band Name Generator/adminchoices.py
+++ Band Name Generator/adminchoices.py
@@ -1,15 +1,19 @@
-import swapsnippet
 choice= input("Choose what function you want me to perform: \n")
+exit= False
 
-if choice == 1:
-    print("Hello:")
-elif choice == 2:
-    print("This is second choice")
-elif choice == 3:
-    print("This is the third choice")
-elif choice == 4:
-    print("This the 4th choice")
-elif choice == 5:
-    print("This is 5th choice")
-else:
-    print("You chose an invalid choice")
+
+while exit != False:
+    print("Do you want to perform another tasks?")
+    if choice == '1':
+        print("Hello:")
+    elif choice == '2':
+        print("This is second choice")
+    elif choice == '3':
+        print("This is the third choice")
+    elif choice == '4':
+        print("This the 4th choice")
+    elif choice == '5':
+        print("This is 5th choice")
+    else:
+        print("You chose an invalid choice")
+
